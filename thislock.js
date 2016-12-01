4ff03e57a1fec252cc735f42fefbfbfa9e9a651c
Stenkilde <hello@stenkil.de>
2016-12-01 08:42:09 +0100

    Added more facts, PogChamp

--- thislock.js
+++ thislock.js
@@ -1,8 +1,11 @@
-var facts = [
-  "Anton Sokolov made this lock",
-  "How many lovely sokolov locks would a sucky sokolov lock if a sucky sokolov locked lovely sokolov locks?",
-  "Anton Sokolov - THE MAN, THE MYTH, THE LEGEND"
-];
+const facts = [
+  'Anton Sokolov made this lock',
+  'How many lovely sokolov locks would a sucky sokolov lock if a sucky sokolov locked lovely sokolov locks?',
+  'THE MAN, THE MYTH, THE LEGEND',
+  'The best locksmith known to man kind',
+  '"I had a flask in my studio. Had I known I\'d be leaving I would have grabbed it." - Anton Sokolov',
+  'An immigrant from the nation of Tyvia'
+]
 
 var factLength = facts.length;
 
