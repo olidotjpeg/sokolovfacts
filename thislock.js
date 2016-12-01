fb63ea6db6bb729b0ee45e27daf5cf5d784350bb
Oliver Stenkilde <olm@tricom.dk>
2016-12-01 09:25:28 +0100

    Updated things a bit

--- thislock.js
+++ thislock.js
@@ -1,22 +1,22 @@
 const facts = [
-  'Anton Sokolov made this lock',
-  'How many lovely sokolov locks would a sucky sokolov lock if a sucky sokolov locked lovely sokolov locks?',
-  'THE MAN, THE MYTH, THE LEGEND',
-  'The best locksmith known to man kind',
-  '"I had a flask in my studio. Had I known I\'d be leaving I would have grabbed it." - Anton Sokolov',
-  'An immigrant from the nation of Tyvia',
-  'Sokolov is obsessed with the Outsider, so much so that he attempts rituals and sacrifices in order to summon him',
-  'Sokolov took on Delilah, as his apprentice in 1828, so that he may pass on his knowledge of locks before his time comes to an end',
-  'Sokolov maintained a longer-lasting relationship with Jessamine Kaldwin',
-  'In his spare time, Sokolov is a drunkard and womanizer, as well as a wishful practitioner of locks'
+    'Anton Sokolov made this lock',
+    'How many lovely sokolov locks would a sucky sokolov lock if a sucky sokolov locked lovely sokolov locks?',
+    'THE MAN, THE MYTH, THE LEGEND',
+    'The best locksmith known to man kind',
+    '"I had a flask in my studio. Had I known I\'d be leaving I would have grabbed it." - Anton Sokolov',
+    'An immigrant from the nation of Tyvia',
+    'Sokolov is obsessed with the Outsider, so much so that he attempts rituals and sacrifices in order to summon him',
+    'Sokolov took on Delilah, as his apprentice in 1828, so that he may pass on his knowledge of locks before his time comes to an end',
+    'Sokolov maintained a longer-lasting relationship with Jessamine Kaldwin',
+    'In his spare time, Sokolov is a drunkard and womanizer, as well as a wishful practitioner of locks'
 ]
 
-var factLength = facts.length;
-
-var result = facts[Math.floor(Math.random() * factLength)]
+const factLength = facts.length;
+// Sorry TChopsSan
+const result = facts[Math.floor(Math.random() * factLength)]
 
 function anton() {
-		document.getElementById('facts').innerHTML = result;
-	}
+    document.getElementById('facts').innerHTML = result;
+}
 
 anton();
